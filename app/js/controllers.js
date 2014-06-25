901f42bf64760ca0be6e528261bf65c5400c1bcd
Wei Ran Guo <kevinguokg@hotmail.com>
2014-06-24 21:22:42 -0700

    prototype version of online random battle

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -30,9 +30,14 @@ app.config(function ($routeProvider){
 			controller: 'splashController',
 			templateUrl: 'partials/splash.html'
 		})
-		.when('/online/:gameId',
+		.when('/random',
 		{
-			controller: 'friendgameController',
+			controller: '',
+			templateUrl: 'partials/onlinegamelobby.html'
+		})
+		.when('/random/:gameId',
+		{
+			controller: '',
 			templateUrl: 'partials/onlinegame.html'
 		})
 		.when('/friend/:gameId',
@@ -268,7 +273,7 @@ app.controller('splashController', function($scope, $location, $window){
 	$scope.newOnlineGame = function(){
 		console.log('Creating new online game');
 		//alert(Math.floor(Math.random() * 99999).toString());
-		$location.path( "/random/" + Math.floor(Math.random() * 99999).toString());
+		$location.path( "/random");
 	};
 	
 	$window.onbeforeunload  = function(){
@@ -277,12 +282,12 @@ app.controller('splashController', function($scope, $location, $window){
 });
 
 app.controller('friendgameController', function($scope, fbURL, $firebase, $location, $routeParams, $window){
-	//$window.close();
 	this.board  = board;
 	this.bigPad = pad;
 	$scope.turn_1 = true;
 	this.winnerDeclared = false;
 	$scope.winnerIs;
+	var player;
 	//alert($routeParams.gameId); //this will alert gameId
 	//alert($location.absUrl());  //this will alert absolute URL of the site in the browser address bar
 	
@@ -301,99 +306,97 @@ app.controller('friendgameController', function($scope, fbURL, $firebase, $locat
 		}
 	};
 	
-	
+	var absURL = $location.absUrl();
+	if(absURL.indexOf("friend")!= -1){
 		
-	fbURL = fbURL + '/friendgames' + '/' +$routeParams.gameId;
-	$scope.tttData = $firebase(new Firebase(fbURL));
-	
-	var gameInit = new Firebase(fbURL+"/gameStatus");
-	var player;
-	gameInit.once('value', function(snapshot) {
-		//alert('gameStatus? ' + snapshot.val());
-		if(snapshot.val()==="pending" ){
-			//Game is already initialized by Player 1, so assigned local player variable to P2 
-			//alert('gameStatus? ' + snapshot.val().toString());
-			player = 1; // 1 is P2
-			//alert(player);
-			$scope.tttData.$update({gameStatus: 'started'});
-			
-		}
-		else if (snapshot.val()==="started"){
-			player = 2; // 2 is a spectator
-			alert("you are just a spectator!");
-		} else{
-			player = 0  // 0 is P1
-			//alert(player);
-			$scope.tttData.$update({gameStatus: 'pending'});
-			
-		}
+		fbURL = fbURL + '/friendgames' + '/' +$routeParams.gameId;
+		$scope.tttData = $firebase(new Firebase(fbURL));
 		
-	});
-	$scope.resetAll();
-	$scope.tttData.$bind($scope,"board");
-
-	$scope.tttData.gameboard = this.board;
-	$scope.tttData.turn =0; //0=P1, 1=P2;
-	$scope.tttData.winnerDeclared =false;
-	//$scope.tttData.gameStatus =true;
-	$scope.tttData.winnerIs ="";
-
-	//$scope.tttData.$save("gameboard");
-	//$scope.tttData.$update({winnerIs: ''});
-	//$scope.tttData.$save("turn");
-	//$scope.tttData.$save("winnerDeclared");
-	//$scope.tttData.$save("gameStatus");
-	
-	//$scope.tttData.$update({gameStatus: 'true'});
-	var turn = $scope.turn_1;
-	$scope.tttData.$on('change', function(){
-
-		$scope.updateActivePad();
-		$scope.updateCells();
-		//check dead case OK
-		//check win
-		if ($scope.tttData.winnerIs === 'P1'){
-			//alert("From AngularFire: Player 1 is the winner!");
-			$scope.winnerIs = 1;
-			$scope.tttData.$unbind();
-		}
-		else if ($scope.tttData.winnerIs === 'P2'){
-			//alert("From AngularFire: Player 2 is the winner!");
-			$scope.winnerIs = 1;
-			$scope.tttData.$unbind();
-		}
-		//turn = ($scope.tttData.turn ===0)? true : false; 
-		if ($scope.tttData.turn ==0){
-			$scope.turn_1 = true;
-		}
-		if ($scope.tttData.turn ==1){
-			$scope.turn_1 = false;
-		}
-	});
-	//$scope.turn_1 = turn;
-	
-	$scope.isGameStarted = function(){
 		var gameInit = new Firebase(fbURL+"/gameStatus");
-		console.log(gameInit.value());
-		gameInit.once('value', function(snapshot) {
-		if(snapshot.val()==="pending" ){
-			//alert("pend");
-			// Game is pending, wait for player 2
-			return 0;
-		}
-		else if (snapshot.val()==="started"){
-			//game started
-			return 1;
-		} else{
-			//other case: maybe left the game
-			console.log(snapshot.val());
-			console.log("A player has left the game!");
-			return 0;
-		}
 		
-	});
-	};
-	
+		gameInit.once('value', function(snapshot) {
+			//alert('gameStatus? ' + snapshot.val());
+			if(snapshot.val()==="pending" ){
+				//Game is already initialized by Player 1, so assigned local player variable to P2 
+				//alert('gameStatus? ' + snapshot.val().toString());
+				player = 1; // 1 is P2
+				//alert(player);
+				$scope.tttData.$update({gameStatus: 'started'});
+				
+			}
+			else if (snapshot.val()==="started"){
+				player = 2; // 2 is a spectator
+				alert("you are just a spectator!");
+			} else{
+				player = 0  // 0 is P1
+				//alert(player);
+				$scope.tttData.$update({gameStatus: 'pending'});
+				
+			}
+			
+		});
+		gameInit.on('child_removed', function(snapshot){
+			alert("Error: Your opponent has left the game!");
+		}, function(err){
+			alert("Error: Some one has left the game!");
+		});
+		$scope.resetAll();
+		$scope.tttData.$bind($scope,"board");
+
+		$scope.tttData.gameboard = this.board;
+		$scope.tttData.turn =0; //0=P1, 1=P2;
+		$scope.tttData.winnerDeclared =false;
+		$scope.tttData.winnerIs ="";
+
+		var turn = $scope.turn_1;
+		$scope.tttData.$on('change', function(){
+
+			$scope.updateActivePad();
+			$scope.updateCells();
+			//check dead case OK
+			//check win
+			if ($scope.tttData.winnerIs === 'P1'){
+				//alert("From AngularFire: Player 1 is the winner!");
+				$scope.winnerIs = 1;
+				$scope.tttData.$unbind();
+			}
+			else if ($scope.tttData.winnerIs === 'P2'){
+				//alert("From AngularFire: Player 2 is the winner!");
+				$scope.winnerIs = 1;
+				$scope.tttData.$unbind();
+			}
+			//turn = ($scope.tttData.turn ===0)? true : false; 
+			if ($scope.tttData.turn ==0){
+				$scope.turn_1 = true;
+			}
+			if ($scope.tttData.turn ==1){
+				$scope.turn_1 = false;
+			}
+		});
+		//$scope.turn_1 = turn;
+		
+		$scope.isGameStarted = function(){
+			var gameInit = new Firebase(fbURL+"/gameStatus");
+			console.log(gameInit.value());
+			gameInit.once('value', function(snapshot) {
+			if(snapshot.val()==="pending" ){
+				//alert("pend");
+				// Game is pending, wait for player 2
+				return 0;
+			}
+			else if (snapshot.val()==="started"){
+				//game started
+				return 1;
+			} else{
+				//other case: maybe left the game
+				console.log(snapshot.val());
+				console.log("A player has left the game!");
+				return 0;
+			}
+			
+		});
+		};
+	}
 	
     this.isOccupy = function(cell){
         if (cell.ownBy===0)
@@ -640,13 +643,17 @@ app.controller('friendgameController', function($scope, fbURL, $firebase, $locat
 	};
 	
 	$scope.quit = function(){
-		$scope.resetAll();
-		$scope.tttData.$remove();
+		if (player==1 || player==0){
+			$scope.resetAll();
+			$scope.tttData.$remove();
+		}
 	}
 	
 	$window.onbeforeunload  = function(){
-		$scope.resetAll();
-		$scope.tttData.$remove();
+		if (player==1 || player==0){
+			$scope.resetAll();
+			$scope.tttData.$remove();
+		}
 	};
 	
 	$window.onhashchange = function(){
@@ -656,8 +663,417 @@ app.controller('friendgameController', function($scope, fbURL, $firebase, $locat
 	
 });
 
-app.controller('onlineController', function(){ 
+app.controller('onlineHandler', function($scope, fbURL, $firebase, $location, $routeParams, $window){ 
+	var absURL = $location.absUrl();
+	var targetPairedID;
+	if(absURL.indexOf("random")!= -1){
+		var randomGameID = Math.floor(Math.random() * 99999).toString();
+		fbURL = fbURL + '/randomgames/waitinglist';
+		var waitingList =new Firebase(fbURL);
+		//waitingList.child(randomGameID).set(randomGameID);
+		
+		waitingList.on('value', function(data){
+			if(data.val() === null){
+				console.log('List item does not exist. Create a new list and a new item. ');
+				waitingList.child(randomGameID).set(randomGameID);
+			} 
+			else{
+				data.forEach(function(child){
+					console.log(child.exportVal());
+					targetPairedID = child.val();
+					
+				});
+				//console.log("targetPairedID = " + targetPairedID);
+				$location.path("/random/" + targetPairedID);
+			}
+		});
+		//console.log(targetPairedID);
+		$location.path("/random/" + targetPairedID);
+	}
+});
 
+app.controller('onlineController', function($scope, fbURL, $firebase, $location, $routeParams, $window){ 
+	this.board  = board;
+	this.bigPad = pad;
+	$scope.turn_1 = true;
+	this.winnerDeclared = false;
+	$scope.winnerIs;
+	var player;
+	//alert($routeParams.gameId); //this will alert gameId
+	//alert($location.absUrl());  //this will alert absolute URL of the site in the browser address bar
+	
+	$scope.resetAll = function(){
+		for(var boardRow=0; boardRow<3; boardRow++){
+			for(var boardCol=0; boardCol<3; boardCol++){			
+				board[boardRow][boardCol][0][0].padActive = 1;
+				//$scope.tttData.gameboard[boardRow][boardCol][0][0].padActive=1;
+				for(var cellRow=0; cellRow<3; cellRow++){
+					for(var cellCol=0; cellCol<3; cellCol++){
+						board[boardRow][boardCol][cellRow][cellCol].ownBy = 0;
+						//$scope.tttData.gameboard[boardRow][boardCol][cellRow][cellCol].ownBy=0;
+					}
+				}
+			}
+		}
+	};
+	
+	var absURL = $location.absUrl();
+	if(absURL.indexOf("random")!= -1){
+		
+		fbURL = fbURL + '/randomgames' + '/' +$routeParams.gameId;
+		$scope.tttData = $firebase(new Firebase(fbURL));
+		
+		var gameInit = new Firebase(fbURL+"/gameStatus");
+		
+		gameInit.once('value', function(snapshot) {
+			//alert('gameStatus? ' + snapshot.val());
+			if(snapshot.val()==="pending" ){
+				//Game is already initialized by Player 1, so assigned local player variable to P2 
+				//alert('gameStatus? ' + snapshot.val().toString());
+				player = 1; // 1 is P2
+				//alert(player);
+				$scope.tttData.$update({gameStatus: 'started'});
+				
+			}
+			else if (snapshot.val()==="started"){
+				player = 2; // 2 is a spectator
+				alert("you are just a spectator!");
+			} else{
+				player = 0  // 0 is P1
+				//alert(player);
+				$scope.tttData.$update({gameStatus: 'pending'});
+				
+			}
+			
+		});
+		gameInit.on('child_removed', function(snapshot){
+			alert("Error: Your opponent has left the game!");
+		}, function(err){
+			alert("Error: Some one has left the game!");
+		});
+		$scope.resetAll();
+		$scope.tttData.$bind($scope,"board");
+
+		$scope.tttData.gameboard = this.board;
+		$scope.tttData.turn =0; //0=P1, 1=P2;
+		$scope.tttData.winnerDeclared =false;
+		$scope.tttData.winnerIs ="";
+
+		var turn = $scope.turn_1;
+		$scope.tttData.$on('change', function(){
+
+			$scope.updateActivePad();
+			$scope.updateCells();
+			//check dead case OK
+			//check win
+			if ($scope.tttData.winnerIs === 'P1'){
+				//alert("From AngularFire: Player 1 is the winner!");
+				$scope.winnerIs = 1;
+				$scope.tttData.$unbind();
+			}
+			else if ($scope.tttData.winnerIs === 'P2'){
+				//alert("From AngularFire: Player 2 is the winner!");
+				$scope.winnerIs = 1;
+				$scope.tttData.$unbind();
+			}
+			//turn = ($scope.tttData.turn ===0)? true : false; 
+			if ($scope.tttData.turn ==0){
+				$scope.turn_1 = true;
+			}
+			if ($scope.tttData.turn ==1){
+				$scope.turn_1 = false;
+			}
+		});
+		//$scope.turn_1 = turn;
+		
+		$scope.isGameStarted = function(){
+			var gameInit = new Firebase(fbURL+"/gameStatus");
+			console.log(gameInit.value());
+			gameInit.once('value', function(snapshot) {
+			if(snapshot.val()==="pending" ){
+				//alert("pend");
+				// Game is pending, wait for player 2
+				return 0;
+			}
+			else if (snapshot.val()==="started"){
+				//game started
+				return 1;
+			} else{
+				//other case: maybe left the game
+				console.log(snapshot.val());
+				console.log("A player has left the game!");
+				return 0;
+			}
+			
+		});
+		};
+	}
+	
+    this.isOccupy = function(cell){
+        if (cell.ownBy===0)
+			{return false;}
+		else
+			{return true;}
+    };
+	
+	this.isWinnerDeclared = function(){
+		return $scope.tttData.winnerDeclared;
+	};
+	
+	this.changePadPlayable = function(pad, row, col){
+		//alert("row: " + row + ";  col: " + col);
+		for(var boardRow=0; boardRow<3; boardRow++){
+			for(var boardCol=0; boardCol<3; boardCol++){
+				if (boardRow== row && boardCol==col){
+					if(this.checkPadFull(pad, row, col)===1){
+						this.freeAllPads();
+						return;
+					}
+					board[boardRow][boardCol][0][0].padActive = 1;
+					$scope.tttData.gameboard[boardRow][boardCol][0][0].padActive=1;
+					//$scope.tttData.$save('gameboard');
+				}
+				else {
+					board[boardRow][boardCol][0][0].padActive = 0;
+					$scope.tttData.gameboard[boardRow][boardCol][0][0].padActive=0;
+					//$scope.tttData.$save('gameboard');
+				}	
+			}
+		}
+	};
+	
+	this.playable = function(pad){
+		var padActive = pad[0][0].padActive;
+		return padActive;
+	};
+
+	this.checkPadFull = function(pad, row, col){
+		var cellCount=0;
+		for(var padRow=0; padRow<3; padRow++){
+			for(var padCol=0; padCol<3; padCol++){
+				if ($scope.tttData.gameboard[row][col][padRow][padCol].ownBy === 0){
+					//alert("pad " + row + ", " + col + "is not full!");
+					return 0;
+				}
+				else{
+					cellCount++;
+				}
+			}
+		}
+		//alert("pad " + row + ", " + col + " is full. : " + cellCount);
+		//alert(cellCount);
+		return cellCount==9? 1:0; 
+	};
+	this.checkLocalPadWinner = function(row, col){
+		//alert(row.toString() + " and "+col.toString());
+		return $scope.tttData.gameboard[row][col][0][0].localWinner;
+	}
+	
+    this.cellOnClick = function(pad, row, col, row_p, col_p){
+		if(this.playable(pad) && !(this.isWinnerDeclared())){
+			console.log("player: " + player);
+			//console.log($scope.turn_1);
+			console.log("turn: " + $scope.tttData.turn);
+			//Setting ownership
+			if (this.isOccupy(pad[row][col])){
+				alert("This is occupied!");
+			}else{
+				if($scope.tttData.turn === 0 && player===0){
+					pad[row][col].Symbol="X";
+					pad[row][col].ownBy=1;
+					$scope.turn_1=false;
+					$scope.tttData.turn =1;
+					$scope.tttData.gameboard[row_p][col_p][row][col].ownBy=1;
+					this.changePadPlayable(pad, row, col);
+					$scope.tttData.$save("gameboard").then(function(){});
+					$scope.tttData.$save("turn");
+
+				}else if ($scope.tttData.turn === 1 && player=== 1){
+					pad[row][col].Symbol="O";
+					pad[row][col].ownBy=-1;
+					$scope.turn_1=true;
+					$scope.tttData.turn =0;
+					$scope.tttData.gameboard[row_p][col_p][row][col].ownBy=-1;
+					this.changePadPlayable(pad, row, col);
+					$scope.tttData.$save("gameboard").then(function(){});
+                    $scope.tttData.$save("turn");
+					
+					
+				}
+				//this.changePadPlayable(pad, row, col);
+			}
+            //if($scope.tttData.gameboard[row_p][col_p][0][0].localWinner == 0){
+			var localResult = this.checkWin(pad);
+			//alert($scope.tttData.gameboard[row_p][col_p][0][0].localWinner);
+			if($scope.tttData.gameboard[row_p][col_p][0][0].localWinner == 0){
+				this.bigPad[row_p][col_p].ownBy = localResult;
+				this.board[row_p][col_p][0][0].localWinner = localResult;
+				$scope.tttData.gameboard[row_p][col_p][0][0].localWinner=localResult;
+				$scope.tttData.$save("gameboard");
+			}
+			var result = this.checkWin(this.bigPad);
+			if (result===1){
+				//alert("Player 1 wins!");
+				this.winnerDeclared = true;
+				$scope.winnerIs =1;
+				$scope.tttData.$update({winnerDeclared: true, winnerIs: 'P1'});
+			}
+			else if (result===-1){
+				//alert("Player 2 wins!");
+				this.winnerDeclared = true;
+				$scope.winnerIs =2;
+				$scope.tttData.$update({winnerIs: 'P2'});
+				$scope.tttData.$update({winnerDeclared: true});
+			}
+		} else if (this.playable(pad) && this.isWinnerDeclared()){
+			if (!this.isOccupy(pad[row][col])){
+				this.changePadPlayable(pad, row, col);
+			}
+		}
+	};
+        
+	this.checkPadWin = function (unit){
+		
+	};
+	
+	this.checkWin = function(unit){
+		var sum = 0;
+		
+		//check rows		
+		for (var i = 0; i < 3; i++){
+			sum = unit[i][0].ownBy+unit[i][1].ownBy+unit[i][2].ownBy;
+			if (sum===3){
+				return 1;
+			}else if(sum===-3){
+				return -1;
+			}
+		}
+            
+		//check cols		
+		for (var i = 0; i < 3; i++){
+			sum = unit[0][i].ownBy+unit[1][i].ownBy+unit[2][i].ownBy;
+			if (sum===3){
+				return 1;
+			}else if(sum===-3){
+				return -1;
+			}
+		}
+		
+		//check left diag
+		sum = unit[0][0].ownBy+unit[1][1].ownBy+unit[2][2].ownBy;
+		if (sum===3){
+			return 1;
+		}else if(sum===-3){
+			return -1;
+		}
+			
+		//check right diag
+		sum = unit[2][0].ownBy+unit[1][1].ownBy+unit[0][2].ownBy;
+		if (sum===3){
+			return 1;
+		}else if(sum===-3){
+			return -1;
+		}
+		
+		return 0;
+	};
+	
+	$scope.rematch = function(){
+		if (confirm("Rematch. Are you sure???")){
+			for(var boardRow=0; boardRow<3; boardRow++){
+				for(var boardCol=0; boardCol<3; boardCol++){			
+					board[boardRow][boardCol][0][0].padActive = 1;
+					$scope.tttData.gameboard[boardRow][boardCol][0][0].padActive=1;
+					$scope.tttData.gameboard[boardRow][boardCol][0][0].localWinner=0;
+					for(var cellRow=0; cellRow<3; cellRow++){
+						for(var cellCol=0; cellCol<3; cellCol++){
+							board[boardRow][boardCol][cellRow][cellCol].ownBy = 0;
+							$scope.tttData.gameboard[boardRow][boardCol][cellRow][cellCol].ownBy=0;
+						}
+					}
+				}
+			}
+			$scope.tttData.winnerDeclared= false;
+			$scope.tttData.$save('winnerDeclared');
+			$scope.tttData.$save('gameboard');
+			$scope.tttData.$update({turn:0, winnerIs: ""});
+		}
+	};
+	
+	$scope.resetAll = function(){
+		for(var boardRow=0; boardRow<3; boardRow++){
+			for(var boardCol=0; boardCol<3; boardCol++){			
+				board[boardRow][boardCol][0][0].padActive = 1;
+				//$scope.tttData.gameboard[boardRow][boardCol][0][0].padActive=1;
+				for(var cellRow=0; cellRow<3; cellRow++){
+					for(var cellCol=0; cellCol<3; cellCol++){
+						board[boardRow][boardCol][cellRow][cellCol].ownBy = 0;
+						//$scope.tttData.gameboard[boardRow][boardCol][cellRow][cellCol].ownBy=0;
+					}
+				}
+			}
+		}
+		//$scope.tttData.winnerDeclared= false;
+		//$scope.tttData.turn= 1;
+		//$scope.tttData.$save('winnerDeclared');
+		//$scope.tttData.$save('gameboard');
+		
+	};
+	
+	this.freeAllPads = function(){
+		for(var boardRow=0; boardRow<3; boardRow++){
+			for(var boardCol=0; boardCol<3; boardCol++){			
+				board[boardRow][boardCol][0][0].padActive = 1;
+				$scope.tttData.gameboard[boardRow][boardCol][0][0].padActive = 1;
+			}
+		}
+	};
+	
+	$scope.updateActivePad = function(){
+		for(var boardRow=0; boardRow<3; boardRow++){
+			for(var boardCol=0; boardCol<3; boardCol++){
+				board[boardRow][boardCol][0][0].padActive = $scope.tttData.gameboard[boardRow][boardCol][0][0].padActive;
+			}
+		}
+	};
+	
+	$scope.updateCells = function(){
+		for(var boardRow=0; boardRow<3; boardRow++){
+			for(var boardCol=0; boardCol<3; boardCol++){			
+
+				for(var cellRow=0; cellRow<3; cellRow++){
+					for(var cellCol=0; cellCol<3; cellCol++){
+						//update cell occupations
+						//if (board[boardRow][boardCol][cellRow][cellCol].ownBy != $scope.tttData.gameboard[boardRow][boardCol][cellRow][cellCol].ownBy){
+							board[boardRow][boardCol][cellRow][cellCol].ownBy = $scope.tttData.gameboard[boardRow][boardCol][cellRow][cellCol].ownBy;
+						//}
+					}
+				}
+			}
+		}
+	};
+	
+	$scope.quit = function(){
+		if (player==1 || player==0){
+			$scope.resetAll();
+			$scope.tttData.$remove();
+		}
+	}
+	
+	$window.onbeforeunload  = function(){
+		if (player==1 || player==0){
+			$scope.resetAll();
+			$scope.tttData.$remove();
+		}
+	};
+	
+	$window.onhashchange = function(){
+		//$scope.resetAll();
+		//$scope.tttData.$remove();
+	};
+	
+
+	/*
 	this.board  = board;
 	this.bigPad = pad;
 	this.turn_1 = true;
@@ -841,8 +1257,9 @@ app.controller('onlineController', function(){
 	server.on('orderToClient', function (order) {
 		onlineCtrl.cellOnClick(order.row, order.col, order.row_p, order.col_p);
 	});
-	
+	*/
 });
+
 
 
 
